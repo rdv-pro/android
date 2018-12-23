6e83b2c85bf9f8c76c652187e76bddd439129d7c
rdv-pro <46091174+rdv-pro@users.noreply.github.com>
2018-12-23 23:49:49 +0300

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -v
-
+#wget https://dl-xda.xposed.info/modules/com.bigsing.changer_v23_25075d.apk
 #adb install ./com.bigsing.changer_v23_25075d.apk
 #adb install ./XposedInstaller_3.1.5-Magisk.apk
 
@@ -9,8 +9,7 @@ snap restart anbox.container-manager
 apt install git -y
 git clone https://github.com/rdv-pro/android.git
 cp -R ./android/system /var/snap/anbox/common/rootfs-overlay
-wget https://dl-xda.xposed.info/modules/com.bigsing.changer_v23_25075d.apk
-cp ./com.bigsing.changer_v23_25075d.apk /var/snap/anbox/common/rootfs-overlay/system/priv-app/com.bigsing.changer_v23_25075d.apk
+
 sudo chown -R 100000:100000 /var/snap/anbox/common/rootfs-overlay
 sudo chmod -R +x /var/snap/anbox/common/rootfs-overlay/system/bin
 sudo chmod -R +x /var/snap/anbox/common/rootfs-overlay/system/xbin
